96e2f46d5db42d23c9a1a51f53fc930cd22549fb
lee <lcjqq99@live.cn>
2023-08-06 17:40:27 +0800

    feat: nav sidebar

--- app/(auth)/(routes)/login/page.tsx
+++
@@ -1,5 +0,0 @@
-const LoginPage = () => {
-    return ( <div>Login Page (UnProtected)</div> );
-}
- 
-export default LoginPage;
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,5 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 const RootPage = () => {
-  return <div>Root Page (Protected)</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default RootPage;
