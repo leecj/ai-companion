6088ea3d66eb15e9173907bf7280a8d5b1f038f6
lee <lcjqq99@live.cn>
2023-08-10 20:07:18 +0800

    feat: hook dialog

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,6 +8,7 @@ import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/theme-toggle";
 import MobileSidebar from "@/components/mobile-sidebar";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 const font = Poppins({
   weight: "600",
@@ -15,6 +16,8 @@ const font = Poppins({
 });
 
 const Navbar = () => {
+  const proModal = useProModal();
+  
   return (
     <div
       className="fixed w-full z-50 flex justify-between
@@ -34,12 +37,12 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="flex items-center gap-x-3">
-        <Button variant="premium" size="sm">
+        <Button onClick={proModal.onOpen} size="sm" variant="premium">
           Upgrade
           <Sparkles className="h-4 w-4 fill-white text-white ml-2"></Sparkles>
         </Button>
         <ModeToggle></ModeToggle>
-        <UserButton afterSignOutUrl="/"/>
+        <UserButton afterSignOutUrl="/" />
       </div>
     </div>
   );
